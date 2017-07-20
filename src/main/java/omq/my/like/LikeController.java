9ed2a7ab74243922e90a648ac971ca8c4b9cbf47
ghost <ghost@Ghost-OU>
2017-07-20 17:07:03 +0800

    点赞功能

--- src/main/java/omq/my/like/LikeController.java
+++ src/main/java/omq/my/like/LikeController.java
@@ -1,9 +1,21 @@
 package omq.my.like;
 
+import com.jfinal.kit.Ret;
+
 import omq.common.controller.BaseController;
 
 public class LikeController extends BaseController{
 	
 	static LikeService srv = new LikeService().me;
+	
+	public void index(){
+		if(notLogin()){
+			renderJson(Ret.fail("msg", "请先登录"));
+			return;
+		}
+		
+		Ret ret = srv.like(getLoginAccountId(), getPara("refType"), getParaToInt("refId"), getParaToBoolean("isAdd")));
+		renderJson(ret);
+	}
 
 }
